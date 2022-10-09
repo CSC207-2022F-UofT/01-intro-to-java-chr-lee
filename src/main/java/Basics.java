0948a83520a314ae9e715d88b8c6d75ad91bb132
chr-lee <chrislee6260@gmail.com>
2022-10-09 06:39:12 -0400

    1. Introduction to Java GitHub Exercise

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println(("Hello World!"));
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for(int curr_count = 10; curr_count >= 0; curr_count--){
+            System.out.println("Current count: " + curr_count);
+        }
 
 
     }
@@ -144,6 +147,12 @@ public class Basics {
 
         // Fill in the rest of the body here
 
+        String[] words = to_split.split(" ");
+
+        for(String s : words){
+            ret.append(s.charAt(0));
+        }
+
         return ret.toString();
     }
 
@@ -170,6 +179,12 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 != 0) {
+                current_sum += arr[i];
+            }
+        }
 
         return current_sum;
     }
